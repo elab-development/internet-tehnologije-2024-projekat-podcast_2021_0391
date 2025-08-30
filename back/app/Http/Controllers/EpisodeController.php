7f808d9efdec2ca40809e8fd7e53741fd0b09fa8
kristinalekic02 <kristinalekic02@gmail.com>
2025-08-30 09:05:08 +0200

    Dodata metoda store za cuvanje epizode podkasta

---
+++ back/app/Http/Controllers/EpisodeController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Episode;
+use App\Models\Podcast;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+use App\Http\Resources\EpisodeResource;
+
+
+class EpisodeController extends Controller
+{
+   
+
+
+    public function store(Request $request)
+    {
+        try{
+            Log::info('Request Data:', $request->all());
+
+
+        $request->validate([
+            'title' => 'required|string',
+            'podcast_id' => 'required|exists:podcasts,id',
+          
+        ]);
+
+
+        $user = Auth::user();
+        $podcast = Podcast::findOrFail($request->podcast_id);
+
+        if ($podcast->creators()->where('user_id', $user->id)->exists() || $user->role=="admin") {
+          
+     
+
+        $file = $this->uploadFile($request->file('file'), $request->title,$podcast);
+        $episode = Episode::create([
+            'title' => $request->title,
+            'date' =>now(),
+            'podcast_id' => $request->podcast_id,
+            'file'=>$file,
+            'type'=>$request->file('file')->getMimeType()
+        ]);
+        return response()->json(['message' => 'The episode and file have been successfully saved'], 201);
+
+    }
+        else{
+            return response()->json([
+                'error' => 'You do not have permission to store episode.',
+            ], 403); 
+        }
+
+        }
+        catch (\Exception $e) {
+            return response()->json(['error' => 'An error occurred while saving the episode and file.'], 500);
+        }
+    
+    }
+    
+    private function uploadFile($file, $title,$podcast)
+    {
+        $originalExtension = $file->getClientOriginalExtension(); 
+        $filename = $title . '.' . $originalExtension;
+        $sanitizedtitle = preg_replace('/[^a-zA-Z0-9_-]/', '_', $podcast->title);
+
+        $podcastPath = 'app/' . $sanitizedtitle;
+        if (!Storage::exists($podcastPath)) {
+             Storage::makeDirectory($podcastPath);
+            }
+
+            $sanitizedtitle = preg_replace('/[^a-zA-Z0-9_-]/', '_', $title);
+            $path = $podcastPath . '/'. $sanitizedtitle;
+            if(!Storage::exists($path))
+            {
+                Storage::makeDirectory($path);
+            }
+        
+        $pathFile = $file->storeAs($path, $filename,"public");
+
+        return Storage::url($pathFile); 
+    }
+    
+
+
+
+
+}
+
--- back/routes/api.php
+++ back/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PodcastController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\EpisodeController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -17,6 +18,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/podcasts',[PodcastController::class,'store']);
     Route::get('/podcasts/{id}',[PodcastController::class,'show']);
     Route::delete('/podcasts/{id}',[PodcastController::class,'destroy']);
+
+
+    Route::post('/episodes',[EpisodeController::class,'store']);
 
 
     Route::get('/categories',[CategoryController::class,'index']);
